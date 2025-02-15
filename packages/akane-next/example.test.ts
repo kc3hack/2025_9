f5ca5e630e04e8d94ded5590328bfe5eccff5da5
Koki Matsumoto <85651386+rokuosan@users.noreply.github.com>
2025-02-15 23:53:09 +0900

    サンプルのテストコードを書く

---
+++ packages/akane-next/example.test.ts
@@ -0,0 +1,8 @@
+// vitest のサンプル
+// vitest がテストケースを一つも見つけられないエラーになるので、そのエラーを回避するためにも書いている。
+// TODO(vitest-example): vitest のサンプルが必要ならこのファイルを削除する
+describe('example', () => {
+  it('should return 1', () => {
+    expect(1).toBe(1);
+  });
+})
