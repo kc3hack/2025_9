26c9175fc29b632dd547f89c97115eb71abcfa34
Koki Matsumoto <85651386+rokuosan@users.noreply.github.com>
2025-02-16 15:34:11 +0900

    GraphQL の定義部分は無視してもらう

--- packages/akane-next/eslint.config.mjs
+++ packages/akane-next/eslint.config.mjs
@@ -11,6 +11,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: [
+      "src/graphql/handler/**/*.ts",
+      "src/app/api/graphql/**/*.ts"
+    ],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off"
+    }
+  }
 ];
 
 export default eslintConfig;
