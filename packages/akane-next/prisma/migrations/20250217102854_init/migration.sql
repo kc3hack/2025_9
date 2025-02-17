452dace3637d563f9c35f96d47e8ebd9f6cf04d7
Koki Matsumoto <85651386+rokuosan@users.noreply.github.com>
2025-02-17 19:50:19 +0900

    prisma 導入

---
+++ packages/akane-next/prisma/migrations/20250217102854_init/migration.sql
@@ -0,0 +1,83 @@
+-- CreateTable
+CREATE TABLE `UserAccount` (
+    `id` BIGINT NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
+    `createdAt` TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+    `updatedAt` TIMESTAMP(6) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Story` (
+    `id` BIGINT NOT NULL,
+    `user_account_id` BIGINT NOT NULL,
+    `title` VARCHAR(255) NOT NULL,
+    `content` TEXT NOT NULL,
+    `image_url` VARCHAR(255) NULL,
+    `type` VARCHAR(255) NOT NULL,
+    `status` VARCHAR(255) NOT NULL DEFAULT 'draft',
+    `difficulty` TINYINT NOT NULL DEFAULT 0,
+    `estimated_time` VARCHAR(255) NULL,
+    `area` VARCHAR(255) NOT NULL,
+    `radius` FLOAT NOT NULL DEFAULT 0,
+    `latitude` FLOAT NOT NULL,
+    `longitude` FLOAT NOT NULL,
+    `pin_class` VARCHAR(255) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Question` (
+    `id` BIGINT NOT NULL,
+    `story_id` BIGINT NOT NULL,
+    `title` VARCHAR(255) NOT NULL,
+    `content` TEXT NOT NULL,
+    `image_url` VARCHAR(255) NULL,
+    `answer` VARCHAR(255) NOT NULL,
+    `priority` INTEGER NOT NULL DEFAULT 0,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Hint` (
+    `id` BIGINT NOT NULL,
+    `title` VARCHAR(255) NOT NULL,
+    `content` TEXT NOT NULL,
+    `image_url` VARCHAR(255) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `QuestionHint` (
+    `id` BIGINT NOT NULL,
+    `question_id` BIGINT NOT NULL,
+    `hint_id` BIGINT NOT NULL,
+    `priority` INTEGER NOT NULL DEFAULT 0,
+    `type` VARCHAR(255) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `StoryProgression` (
+    `id` BIGINT NOT NULL,
+    `user_account_id` BIGINT NOT NULL,
+    `story_id` BIGINT NOT NULL,
+    `status` VARCHAR(255) NOT NULL DEFAULT 'started',
+    `current_question_id` BIGINT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `HintInventory` (
+    `id` BIGINT NOT NULL,
+    `user_account_id` BIGINT NOT NULL,
+    `hint_id` BIGINT NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
