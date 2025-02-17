2d1b5eda9e1b08beeb39222c6d46314f2809838f
Koki Matsumoto <85651386+rokuosan@users.noreply.github.com>
2025-02-17 21:24:05 +0900

    GraphQLErrorでエラーを投げる

--- packages/akane-next/src/graphql/resolver/query/user.ts
+++ packages/akane-next/src/graphql/resolver/query/user.ts
@@ -1,17 +1,19 @@
 import prisma from "@/prisma/client";
 import { QueryUserArgs, User } from "../../../../apollo/__generated__/server/resolvers-types";
+import { GraphQLError } from "graphql";
 
 export const UserResolver = async (_: any, args: QueryUserArgs) => {
-
-  const all = await prisma.userAccount.findMany();
-  console.log(all);
   const user = await prisma.userAccount.findUnique({
     where: {
       id: BigInt(args.id)
     }
   });
   if (!user) {
-    throw new Error('User not found');
+    throw new GraphQLError('ユーザーが見つかりませんでした', {
+      extensions: {
+        code: 'USER_NOT_FOUND'
+      }
+    });
   }
 
   return { id: `${user?.id}`, name: user?.name as string } as User;
