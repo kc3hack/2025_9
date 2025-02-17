4d9a1809398987d445343a848ed694cbaeee68ba
Koki Matsumoto <85651386+rokuosan@users.noreply.github.com>
2025-02-17 23:17:02 +0900

    fix: typo

--- packages/akane-next/prisma/migrations/20250217135039_init/migration.sql
+++ packages/akane-next/prisma/migrations/20250217135039_init/migration.sql
@@ -3,7 +3,7 @@ CREATE TABLE `UserAccount` (
     `id` BIGINT NOT NULL,
     `name` VARCHAR(255) NOT NULL,
     `created_at` TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-    `updatedt` TIMESTAMP(6) NOT NULL,
+    `updated_at` TIMESTAMP(6) NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
