c20f8e788d776279671ead0175a4faafce9a8d2b
Kenta Goto <kenta9736@gmail.com>
2025-02-18 09:31:46 +0900

    抜けてたresolverを追加

--- packages/akane-next/src/graphql/server.ts
+++ packages/akane-next/src/graphql/server.ts
@@ -1,22 +1,23 @@
-import { startServerAndCreateNextHandler } from '@as-integrations/next';
-import { ApolloServer } from '@apollo/server';
-import { NextRequest } from 'next/server';
-import { Resolvers } from '../../apollo/__generated__/server/resolvers-types';
-import { typeDefs } from './typedefs';
-import { UserResolver } from './resolver/query/user';
-
+import { startServerAndCreateNextHandler } from "@as-integrations/next";
+import { ApolloServer } from "@apollo/server";
+import { NextRequest } from "next/server";
+import { Resolvers } from "../../apollo/__generated__/server/resolvers-types";
+import { typeDefs } from "./typedefs";
+import { UserResolver } from "./resolver/query/user";
+import { GreetResolver } from "./resolver/query/greet";
 const server = new ApolloServer<Resolvers>({
   typeDefs,
   // Resolver の定義はここで行い、ハンドラとの関連付けを行う
   resolvers: {
     Query: {
       user: UserResolver,
-    }
+      greet: GreetResolver,
+    },
   } as Resolvers,
 });
 
 const GraphAPIServer = startServerAndCreateNextHandler<NextRequest>(server, {
-  context: async req => ({ req, user: {id: 'id-1'} }),
+  context: async (req) => ({ req, user: { id: "id-1" } }),
 });
 
 export default GraphAPIServer;
