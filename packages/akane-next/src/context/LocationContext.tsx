b69ddc9492fc5735b6c01fa194cfaadaf77bcf77
年輪 <nenrin.yearring@gmail.com>
2025-02-20 05:24:10 +0900

    feat: mapに位置情報を乗せる & 位置情報とmapのcontextを

---
+++ packages/akane-next/src/context/LocationContext.tsx
@@ -0,0 +1,63 @@
+"use client";
+import React, { createContext, useContext, useState, useEffect, ReactNode } from 'react';
+
+interface LocationContextProps {
+    position: GeolocationPosition | null;
+    setPosition: React.Dispatch<React.SetStateAction<GeolocationPosition | null>>;
+}
+
+const LocationContext = createContext<LocationContextProps | undefined>(undefined);
+
+export const useLocation = () => {
+    const context = useContext(LocationContext);
+    if (!context) {
+        throw new Error('useLocation must be used within a LocationProvider');
+    }
+    return context;
+};
+
+export const LocationProvider = ({ children }: { children: ReactNode }) => {
+    const [position, setPosition] = useState<GeolocationPosition | null>(null);
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition((position) => {
+        setPosition(position);
+    });
+
+        const watchId = navigator.geolocation.watchPosition(
+        setPosition,
+        (error) => {
+            switch (error.code) {
+            case error.PERMISSION_DENIED:
+                alert('位置情報の取得が許可されていません');
+                break;
+            case error.POSITION_UNAVAILABLE:
+                alert('位置情報が利用できません');
+                break;
+            case error.TIMEOUT:
+                alert('位置情報の取得にタイムアウトしました');
+                break;
+            default:
+                alert('位置情報の取得中にエラーが発生しました');
+                break;
+            }
+            setPosition(null);
+        },
+        {
+            enableHighAccuracy: true,
+            timeout: 15000,
+            maximumAge: 0,
+        }
+        );
+
+        return () => {
+            navigator.geolocation.clearWatch(watchId);
+        };
+    }, []);
+
+    return (
+        <LocationContext.Provider value={{ position, setPosition }}>
+            {children}
+        </LocationContext.Provider>
+    );
+};
