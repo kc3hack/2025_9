937f93017798f2fc4d32584dfd6b7325f6c67940
Koki Matsumoto <85651386+rokuosan@users.noreply.github.com>
2025-02-22 10:37:59 +0900

    Linter + Formatter として Biome を導入

--- packages/akane-next/eslint.config.mjs
+++ packages/akane-next/eslint.config.mjs
@@ -1,25 +1,26 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    files: [
-      "src/graphql/resolver/**/*.ts",
-      "src/app/api/graphql/**/*.ts"
-    ],
-    rules: {
-      "@typescript-eslint/no-explicit-any": "off"
-    }
-  }
+	...compat.extends("next/core-web-vitals", "next/typescript"),
+	{
+		files: [
+			"src/graphql/resolver/**/*.ts",
+			"src/app/api/graphql/**/*.ts",
+			"src/**/*.test.ts",
+		],
+		rules: {
+			"@typescript-eslint/no-explicit-any": "off",
+		},
+	},
 ];
 
 export default eslintConfig;
