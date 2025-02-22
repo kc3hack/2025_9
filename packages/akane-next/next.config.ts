e0b7bc5753059024b396aca4e912e0b3fd297f16
Koki Matsumoto <85651386+rokuosan@users.noreply.github.com>
2025-02-23 03:30:22 +0900

    allowedOrigins にすべてを書く

--- packages/akane-next/next.config.ts
+++ packages/akane-next/next.config.ts
@@ -4,7 +4,17 @@ const nextConfig: NextConfig = {
 	experimental: {
 		optimizePackageImports: ["@chakra-ui/react"],
 		serverActions: {
-			allowedOrigins: ["local.akane.yaken.org", "admin.local.akane.yaken.org"],
+			allowedOrigins: [
+				// 本番環境
+				"akane.yaken.org",
+				"admin-akane.yaken.org",
+				// ステージング環境
+				"st-akane.yaken.org",
+				"admin-st-akane.yaken.org",
+				// ローカル環境
+				"local.akane.yaken.org",
+				"admin.local.akane.yaken.org",
+			],
 		},
 	},
 };
