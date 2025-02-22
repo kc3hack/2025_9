473d042ef3b643a9d24e29ecf087af869f426580
Koki Matsumoto <85651386+rokuosan@users.noreply.github.com>
2025-02-22 10:45:21 +0900

    Server Actions で許可するドメインを指定

--- packages/akane-next/next.config.ts
+++ packages/akane-next/next.config.ts
@@ -1,11 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    optimizePackageImports: [
-      "@chakra-ui/react",
-    ]
-  }
+	experimental: {
+		optimizePackageImports: ["@chakra-ui/react"],
+		serverActions: {
+			allowedOrigins: ["local.akane.yaken.org", "admin.local.akane.yaken.org"],
+		},
+	},
 };
 
 export default nextConfig;
