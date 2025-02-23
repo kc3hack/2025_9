61a40231d78d13c21d715739bd947b2d8ef7807f
Kenta Goto <kenta9736@gmail.com>
2025-02-23 18:14:59 +0900

    アイコンの調整をしました

--- packages/akane-next/src/app/(main)/mypage/page.tsx
+++ packages/akane-next/src/app/(main)/mypage/page.tsx
@@ -2,29 +2,44 @@ import StoryGallary from "@/components/mypage/StoryGallery";
 import { Avatar, AvatarGroup, Box, Stack, Tabs, Text } from "@chakra-ui/react";
 
 export default function MyPage() {
-	// TODO：storyが実装されたらここからフェッチする
+	// TODO：storyのAPIが実装されたらここからフェッチする
 	// const STORY_ENDPOINT = "";
 	// const story = fetch(Story_ENDPOINT);
 	// const avatarIconURL = "https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-	const variant = "hoge";
-	const name = "hoge";
+	const name = "名前";
 	return (
 		<div>
-			<Box width="full" display="flex" justifyContent="center" marginY="16">
+			<Box
+				width="full"
+				display="flex"
+				justifyContent="center"
+				marginTop="20"
+				marginBottom="5"
+			>
 				<div>
-					<AvatarGroup>
-						<Avatar.Root shape="full" size="2xl" variant="subtle">
-							<Avatar.Fallback name={variant} />
-							{/* アイコン画像 */}
-							{/* <Avatar.Image src={AvatarIconURL} /> */}
-						</Avatar.Root>
-					</AvatarGroup>
+					<Box width="full" display="flex" justifyContent="center">
+						<AvatarGroup>
+							<Avatar.Root
+								shape="full"
+								size="full"
+								variant="outline"
+								border="none"
+								bgColor="#EADDFF"
+							>
+								<Box margin="5">
+									<Avatar.Fallback fontSize="6xl" name={name} color="#6750A4" />
+								</Box>
+								{/* アイコン画像 */}
+								{/* <Avatar.Image src={AvatarIconURL} /> */}
+							</Avatar.Root>
+						</AvatarGroup>
+					</Box>
 					<Stack alignItems="center">
-						<Text>{name}</Text>
+						<Text fontSize="3xl">{name}</Text>
 					</Stack>
 				</div>
 			</Box>
-			<Stack width="full">
+			<Stack width="full" maxWidth="600px" marginX="auto">
 				<Tabs.Root defaultValue={"tab1"} variant="line" colorPalette="purple">
 					<Tabs.List
 						display="grid"
