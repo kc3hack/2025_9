edd3da676500a7ac7c4a317dbaf68adff353fbaa
taiseiue <taisei@wsoft.ws>
2025-02-24 11:51:36 +0900

    admin画面でスクロールできないのを修正

--- packages/akane-next/src/app/admin/layout.tsx
+++ packages/akane-next/src/app/admin/layout.tsx
@@ -3,33 +3,38 @@ import type { Metadata, Viewport } from "next";
 import Script from "next/script";
 
 export const metadata: Metadata = {
-	title: "Akane 管理画面",
-	description: "Akane 管理画面です。操作には注意してください。",
+  title: "Akane 管理画面",
+  description: "Akane 管理画面です。操作には注意してください。",
 };
 
 export const viewport: Viewport = {
-	width: "device-width",
-	initialScale: 1.0,
-	maximumScale: 1.0,
+  width: "device-width",
+  initialScale: 1.0,
+  maximumScale: 1.0,
+};
+
+export const scrollableStyle = {
+  overflow: "scroll",
+  height: "100%",
 };
 
 export default function AdminRootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="ja" suppressHydrationWarning>
-			<body>
-				<AdminPageLayout>{children}</AdminPageLayout>
-			</body>
+  return (
+    <html lang="ja" suppressHydrationWarning>
+      <body style={scrollableStyle} suppressHydrationWarning>
+        <AdminPageLayout>{children}</AdminPageLayout>
+      </body>
 
-			<Script
-				src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-				integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-				crossOrigin="anonymous"
-				strategy="afterInteractive"
-			/>
-		</html>
-	);
+      <Script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossOrigin="anonymous"
+        strategy="afterInteractive"
+      />
+    </html>
+  );
 }
--- packages/akane-next/src/components/admin/layout.tsx
+++ packages/akane-next/src/components/admin/layout.tsx
@@ -3,16 +3,16 @@ import type React from "react";
 import { AdminNavbar } from "./navbar";
 
 export const AdminPageLayout = ({ children }: React.PropsWithChildren) => {
-	return (
-		<div>
-			<AdminNavbar />
-			<main className="container">{children}</main>
-		</div>
-	);
+  return (
+    <div>
+      <AdminNavbar />
+      <main className="container">{children}</main>
+    </div>
+  );
 };
 
 export const AdminPageLayoutDecorator = (Story: React.FC) => (
-	<AdminPageLayout>
-		<Story />
-	</AdminPageLayout>
+  <AdminPageLayout>
+    <Story />
+  </AdminPageLayout>
 );
